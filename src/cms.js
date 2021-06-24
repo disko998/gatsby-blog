845f5fd5f4aaa35e317a810819807757c2e5f9f3
Stefan Diskic <disko998@gmail.com>
2021-06-25 01:38:17 +0200

    module path

--- src/cms.js
+++ src/cms.js
@@ -3,7 +3,7 @@ import CMS from "netlify-cms-app"
 
 import { BlogPost } from "./components/elements/blog-post"
 
-const BlogPostPreview = ({ entry, widgetFor }) => {
+const BlogPostPreview = ({ widgetFor }) => {
   return <BlogPost dangerouslySetInnerHTML={{ __html: widgetFor("body") }} />
 }
 CMS.registerPreviewTemplate("article", BlogPostPreview)
