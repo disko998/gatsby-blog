2c7c4743aac9b61ba9a8e2979535cf35af6e9b58
Stefan Diskic <disko998@gmail.com>
2021-06-25 02:17:21 +0200

    fix path

--- src/cms.js
+++ src/cms.js
@@ -3,8 +3,7 @@ import CMS from "netlify-cms-app"
 import { ThemeProvider } from "styled-components"
 
 import { BlogPost } from "./components/elements/blog-post"
-import GlobalStyles from "./styles/global"
-import theme from "./src/styles/theme"
+import { GlobalStyles, theme } from "./styles"
 
 const BlogPostPreview = ({ widgetFor }) => {
   return (
