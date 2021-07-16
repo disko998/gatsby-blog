8ae0c04915e15312ad1a2c33db86c9024e070c11
Stefan Diskic <disko998@gmail.com>
2021-07-16 02:18:04 +0200

    hotfix

--- src/cms.js
+++ src/cms.js
@@ -4,7 +4,7 @@ import { ThemeProvider } from "styled-components"
 import styled from "styled-components"
 
 import { CSSInjector, GlobalStyles, theme } from "./styles"
-// import BlogPost from "./components/widgets/blog/BlogPost"
+import BlogPost from "./components/widgets/blog/BlogPost"
 
 const BlogPostPreview = ({ widgetFor, entry }) => {
   return (
@@ -26,78 +26,3 @@ const BlogPostPreview = ({ widgetFor, entry }) => {
 }
 
 CMS.registerPreviewTemplate("blog", BlogPostPreview)
-
-const BlogContent = styled.article`
-  width: 100%;
-  font-size: 18px;
-  line-height: 1.7;
-
-  & > * {
-    margin-top: 1.5rem;
-  }
-  h1,
-  h2,
-  h3,
-  h4,
-  h5,
-  h6 {
-    margin-top: ${p => p.theme.spacing.xLarge};
-  }
-
-  blockquote {
-    position: relative;
-    padding: 32px 20px 32px 64px;
-    font-size: 1.875rem;
-    line-height: 38px;
-    font-weight: 900;
-    color: ${p => p.theme.colors.light};
-
-    &::before {
-      content: "\\201C";
-      position: absolute;
-      top: 0.59em;
-      left: -0.01em;
-      font-size: 4em;
-      color: ${p => p.theme.colors.main};
-    }
-  }
-
-  a {
-    color: ${p => p.theme.colors.main};
-    text-decoration: none;
-
-    &:hover {
-      text-decoration: underline;
-    }
-  }
-
-  strong {
-    color: ${p => p.theme.colors.light};
-  }
-
-  ul,
-  ol {
-    margin-left: ${p => p.theme.spacing.medium};
-
-    li {
-      padding-left: ${p => p.theme.spacing.xSmall};
-      margin: ${p => p.theme.spacing.xSmall} 0;
-    }
-  }
-
-  .gatsby-resp-iframe-wrapper {
-    margin: ${p => p.theme.spacing.large} 0;
-    margin-bottom: ${p => p.theme.spacing.large} !important;
-  }
-
-  .gatsby-resp-image-link {
-    border-radius: 16px;
-    overflow: hidden;
-    max-height: 600px;
-
-    img {
-      border-radius: 16px;
-      object-fit: cover;
-    }
-  }
-`
