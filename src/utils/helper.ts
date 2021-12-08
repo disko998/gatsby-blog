517a774056563bd6e52374576f2560c2fb919e5a
disko998 <disko998@gmail.com>
2021-12-08 18:29:38 +0100

    remove console log

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -7,7 +7,6 @@ export const isBrowser = () => typeof window !== "undefined"
 export const __DEV__ = process.env.GATSBY_ENV === "development"
 
 export const shortDescription = (text: string) => {
-  console.log(text.length)
   if (text.length > MAX_DESC_LENGTH) {
     return `${text.substring(0, MAX_DESC_LENGTH)}...`
   }
